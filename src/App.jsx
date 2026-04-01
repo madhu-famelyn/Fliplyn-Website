e394a71e7b281cf7d86870b88c6b2d4008f358cb
madhu-famelyn <madhu@famelyn.com>
2026-04-01 10:01:31 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import OrderPage from "./pages/OrderPage/OrderPage";
 import HeroSection from "./pages/OrderPage/KanchiOrder/KanchiOrder";
+import PolicyReview from "./pages/Home/sections/PolicyReviewPage/PolicyReviewPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/b2b/gowrapalladium" element={<OrderPage />} />
         <Route path="/b2c/kanchi/kondapur" element={<HeroSection />} />
+        <Route path="/policy" element={<PolicyReview />} />
 
       </Routes>
     </Router>
--- src/pages/Home/sections/HeroSection/header.jsx
+++ src/pages/Home/sections/HeroSection/header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import CTAButton from "../../../../components/CTAButton/CTAButton";
 import "./HeroSection.css";
 
@@ -48,6 +49,16 @@ const Header = () => {
                 <a href="#resources" onClick={() => setIsMobileMenuOpen(false)}>
                   Resources
                 </a>
+              </li>
+
+              {/* ✅ NEW POLICY LINK */}
+              <li>
+                <Link
+                  to="/policy"
+                  onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  Policy
+                </Link>
               </li>
             </ul>
 
